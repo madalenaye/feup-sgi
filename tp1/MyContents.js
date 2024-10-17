f3d5ba1913ac1de46e681406253043d1ae4a2d30
up202402793 <up202402793@edu.fe.up.pt>
2024-10-17 10:04:23 +0100

    curved

--- tp1/MyContents.js
+++ tp1/MyContents.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { MyAxis } from './MyAxis.js';
+import { MyNurbsBuilder } from './MyNurbsBuilder.js';
 
 /**
  *  This class contains the contents of out application
@@ -47,108 +48,212 @@ class MyContents  {
      * initializes the contents
      */
     init() {
-       
-        // create once 
+
+
+        // create once
+
         if (this.axis === null) {
+
             // create and attach the axis to the scene
+
             this.axis = new MyAxis(this)
+
             this.app.scene.add(this.axis)
+
         }
 
-        // add a point light on top of the model
-        const pointLight = new THREE.PointLight( 0xffffff, 500, 0 );
-        pointLight.position.set( 0, 20, 0 );
-        this.app.scene.add( pointLight );
 
-        // add a point light helper for the previous point light
-        const sphereSize = 0.5;
-        const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
-        this.app.scene.add( pointLightHelper );
+        // variables to hold the curves
 
-        // add an ambient light
-        const ambientLight = new THREE.AmbientLight( 0x555555 );
-        this.app.scene.add( ambientLight );
+        this.polyline = null
+        this.quadraticBezierCurve = null
+        this.cubicBezierCurve = null
+        this.catmullRomCurve3 = null
 
-        this.buildBox()
+        // number of samples to use for the curves (not for polyline)
+
+        this.numberOfSamples = 16
+
+
+        // hull material and geometry
+
+        this.hullMaterial =
+
+            new THREE.MeshBasicMaterial( {color: 0xffffff,
+
+                    opacity: 3.50, transparent: true} );
+
+       
+
+        // curve recomputation
+
+        this.recompute();
+
+    }
+
+
+
+    // Deletes the contents of the line if it exists and recreates them
+
+    recompute() {
+
+        if (this.polyline !== null) this.app.scene.remove(this.polyline)
+
+        this.initPolyline()
+
+        if (this.quadraticBezierCurve !== null)
+
+            this.app.scene.remove(this.quadraticBezierCurve)
+
+        if (this.cubicBezierCurve !==null)
+
+            this.app.scene.remove(this.cubicBezierCurve)
+
+
+        //this.initQuadraticBezierCurve()
+
+        if (this.catmullRomCurve3 !== null)
+
+            this.app.scene.remove(this.catmullRomCurve3)
         
-        // Create a Plane Mesh with basic material
-        
-        let plane = new THREE.PlaneGeometry( 10, 10 );
-        this.planeMesh = new THREE.Mesh( plane, this.planeMaterial );
-        this.planeMesh.rotation.x = -Math.PI / 2;
-        this.planeMesh.position.y = -0;
-        this.app.scene.add( this.planeMesh );
-    }
-    
-    /**
-     * updates the diffuse plane color and the material
-     * @param {THREE.Color} value 
-     */
-    updateDiffusePlaneColor(value) {
-        this.diffusePlaneColor = value
-        this.planeMaterial.color.set(this.diffusePlaneColor)
-    }
-    /**
-     * updates the specular plane color and the material
-     * @param {THREE.Color} value 
-     */
-    updateSpecularPlaneColor(value) {
-        this.specularPlaneColor = value
-        this.planeMaterial.specular.set(this.specularPlaneColor)
-    }
-    /**
-     * updates the plane shininess and the material
-     * @param {number} value 
-     */
-    updatePlaneShininess(value) {
-        this.planeShininess = value
-        this.planeMaterial.shininess = this.planeShininess
-    }
-    
-    /**
-     * rebuilds the box mesh if required
-     * this method is called from the gui interface
-     */
-    rebuildBox() {
-        // remove boxMesh if exists
-        if (this.boxMesh !== undefined && this.boxMesh !== null) {  
-            this.app.scene.remove(this.boxMesh)
-        }
-        this.buildBox();
-        this.lastBoxEnabled = null
-    }
-    
-    /**
-     * updates the box mesh if required
-     * this method is called from the render method of the app
-     * updates are trigered by boxEnabled property changes
-     */
-    updateBoxIfRequired() {
-        if (this.boxEnabled !== this.lastBoxEnabled) {
-            this.lastBoxEnabled = this.boxEnabled
-            if (this.boxEnabled) {
-                this.app.scene.add(this.boxMesh)
-            }
-            else {
-                this.app.scene.remove(this.boxMesh)
-            }
-        }
+        this.initCatmullRomCurve3()
+
     }
 
+
+    
+
+    drawHull(position, points) {
+
+       
+
+        const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+        let line = new THREE.Line( geometry, this.hullMaterial );
+
+        // set initial position
+
+        line.position.set(position.x,position.y,position.z)
+
+        this.app.scene.add( line );
+
+    }
+
+    
+
+
+    initPolyline() {
+
+
+        // define vertex points
+
+        let points = [
+
+            new THREE.Vector3( -0.6, -0.6, 0.0 ), // starting point
+    
+            new THREE.Vector3( -0.6,  0.6, 0.0 ), // control point
+    
+            new THREE.Vector3(  0.6, -0.6, 0.0 ),  // ending point
+
+            new THREE.Vector4(0.6, 0.6, 0.0)
+    
+        ]
+
+
+        let position = new THREE.Vector3(-4,4,0)
+
+        this.drawHull(position, points);
+
+
+        // define geometry
+
+        const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+
+        // create the line from material and geometry
+
+        this.polyline = new THREE.Line( geometry,
+
+            new THREE.LineBasicMaterial( { color: 0xff0000 } ) );
+
+
+        // set initial position
+
+        this.polyline.position.set(position.x,position.y,position.z)
+
+
+        // add the line to the scene
+
+        this.app.scene.add( this.polyline );
+
+    }
+
+    initCatmullRomCurve3() {
+
+
+        let points = [
+    
+            new THREE.Vector3( -0.6, 0, 0 ),
+
+            new THREE.Vector3(  -0.3,0.6,0.3 ),
+
+            new THREE.Vector3(  0,0,0 ),
+
+            new THREE.Vector3( 0.3,-0.6,0.3 ),
+
+            new THREE.Vector3 ( 0.6,0,0),
+
+            new THREE.Vector3 ( 0.9,0.6,0.3),
+            
+            new THREE.Vector3 ( 1.2,0,0)
+    
+        ]
+
+    
+            let position = new THREE.Vector3(-2,4,0)
+    
+            this.drawHull(position, points);
+    
+    
+    
+    
+        let curve =
+    
+            new THREE.CatmullRomCurve3(points)
+    
+        // sample a number of points on the curve
+    
+        let sampledPoints = curve.getPoints( this.numberOfSamples );
+    
+    
+        this.curveGeometry =
+    
+                new THREE.BufferGeometry().setFromPoints( sampledPoints )
+    
+        this.lineMaterial = new THREE.LineBasicMaterial( { color: 0x00ff00 } )
+    
+        this.lineObj = new THREE.Line( this.curveGeometry, this.lineMaterial )
+    
+        this.lineObj.position.set(position.x,position.y,position.z)
+    
+        this.app.scene.add( this.lineObj );
+    
+    
+    }
+
+
     /**
+
      * updates the contents
-     * this method is called from the render method of the app
-     * 
-     */
-    update() {
-        // check if box mesh needs to be updated
-        this.updateBoxIfRequired()
 
-        // sets the box mesh position based on the displacement vector
-        this.boxMesh.position.x = this.boxDisplacement.x
-        this.boxMesh.position.y = this.boxDisplacement.y
-        this.boxMesh.position.z = this.boxDisplacement.z
-        
+     * this method is called from the render method of the app
+
+     *
+
+     */
+
+    update() {    
+
     }
 
 }
