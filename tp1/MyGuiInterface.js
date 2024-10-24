6b29fa1ea08dc4919b5a85cae15ce8d5926b8874
up202402793 <up202402793@edu.fe.up.pt>
2024-10-24 09:43:04 +0100

    Shadow projections

--- tp1/MyGuiInterface.js
+++ tp1/MyGuiInterface.js
@@ -29,6 +29,7 @@ class MyGuiInterface  {
      * Initialize the gui interface
      */
     init() {
+        /*
         // add a folder to the gui interface for the box
         const boxFolder = this.datgui.addFolder( 'Box' );
         // note that we are using a property from the contents object 
@@ -43,13 +44,14 @@ class MyGuiInterface  {
             'diffuse color': this.contents.diffusePlaneColor,
             'specular color': this.contents.specularPlaneColor,
         };
+        */
 
         // adds a folder to the gui interface for the plane
-        const planeFolder = this.datgui.addFolder( 'Plane' );
+        /*const planeFolder = this.datgui.addFolder( 'Plane' );
         planeFolder.addColor( data, 'diffuse color' ).onChange( (value) => { this.contents.updateDiffusePlaneColor(value) } );
         planeFolder.addColor( data, 'specular color' ).onChange( (value) => { this.contents.updateSpecularPlaneColor(value) } );
         planeFolder.add(this.contents, 'planeShininess', 0, 1000).name("shininess").onChange( (value) => { this.contents.updatePlaneShininess(value) } );
-        planeFolder.open();
+        planeFolder.open();*/
 
         // adds a folder to the gui interface for the camera
         const cameraFolder = this.datgui.addFolder('Camera')
