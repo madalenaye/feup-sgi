aeadff295a4a9384321883c7a46fac922d41879c
up202402793 <up202402793@edu.fe.up.pt>
2024-11-03 11:53:19 +0000

    ceiling light shadows

--- tp1/objects/CeilingLight.js
+++ tp1/objects/CeilingLight.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { shadowDefinitions } from '../utils/ShadowDefinitions.js'; 
 
 class CeilingLight extends THREE.Object3D{
     constructor(lightColor, innerRadius, outerRadius, lightIntensity){
@@ -32,9 +33,11 @@ class CeilingLight extends THREE.Object3D{
         const geometry = new THREE.ShapeGeometry(shape);
         this.lightMaterial = new THREE.MeshBasicMaterial({ color: "#cccccc", side: THREE.DoubleSide, opacity: 0.7 });
         this.lightMesh = new THREE.Mesh(geometry, this.lightMaterial);
+        shadowDefinitions.objectShadow(this.lightMesh);
         this.add(this.lightMesh);
 
         this.light = new THREE.PointLight(this.lightColor, this.lightIntensity, 3, 2);
+        shadowDefinitions.propertiesLightShadow(this.light);
         this.light.position.y = 0.02;
         this.add(this.light);
         
