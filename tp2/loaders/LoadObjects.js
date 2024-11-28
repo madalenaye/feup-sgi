72045c976985f4f8738b83ed4d5b4cbd1134feab
up202402793 <up202402793@edu.fe.up.pt>
2024-11-28 11:16:28 +0000

    correct traverseDFS

--- tp2/loaders/LoadObjects.js
+++ tp2/loaders/LoadObjects.js
@@ -4,6 +4,33 @@ export const loadObjects = {
 
     loadObjects(rootNode, listObjects, organizeMaterials){
 
+        function traverseDFS(node, parentId = null) {
+            if (!node) return;
+
+            if (parentId) {
+                console.log(`Parent: ${parentId}`);
+            }
+
+            if (node.type === 'pointlight' || node.type === 'spotlight' || node.type === 'directionallight') {
+                console.log(`Light with type: ${node.type}`);
+                return;
+            }
+
+            if(node.type === 'primitive'){
+                console.log(`Primitive with type: ${node.subtype}`)
+                return;
+            }
+
+            if (node.id) {
+                console.log(`ID: ${node.id}`);
+            }
+
+            if (node.children && Array.isArray(node.children)) {
+                node.children.forEach(childNode => {traverseDFS(childNode, node.id || null);});
+            }
+        }
         
+        const rootObject = listObjects[rootNode];
+        traverseDFS(rootObject);
     }
 }
