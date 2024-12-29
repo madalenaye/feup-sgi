59b2cbdd30596ec8e674f109ecc1b429dd02c539
Madalena Ye <mads1040@gmail.com>
2024-12-29 13:57:31 +0000

    basket strings

--- tp3/objects/MyBalloon.js
+++ tp3/objects/MyBalloon.js
@@ -10,14 +10,14 @@ class MyBalloon extends THREE.Object3D {
         this.height = 10;
 
         this.material = material;
+        this.buildBalloon();
+    }
+    buildBalloon() {
 
         this.groupBalloon = new THREE.Group();
 
-        this.buildBasket();
-        this.add(this.groupBalloon);
-    }
-    buildBasket() {
-        
+        /* Basket */
+
         // Texture
         this.basketTexture = new THREE.TextureLoader().load('./scenes/textures/basket.png');
         this.basketTexture.wrapS = THREE.RepeatWrapping;
@@ -55,9 +55,30 @@ class MyBalloon extends THREE.Object3D {
         this.basketBase.position.set(0, -0.02, 0);
         this.basketGroup.add(this.basketBase);
 
-        // Basket
+        /* Basket Strings */
+        const numStrings = 5;
+        const stringRadius = 0.05;
+        const stringHeight = 1.5;
+
+        this.stringGroup = new THREE.Group();
+        const angleStep = 2 * Math.PI / numStrings;
+
+        for (let i = 0; i < numStrings; i++){
+            const angle = i * angleStep;
+            const x = Math.cos(angle) * (this.radius/4 - 0.1);
+            const z = Math.sin(angle) * (this.radius/4 - 0.1);
+
+            const stringGeometry = new THREE.CylinderGeometry(stringRadius, stringRadius, stringHeight, 32);
+            const string = new THREE.Mesh(stringGeometry, this.basketMaterial);
+            string.position.set(x, extrudeSettings.depth + stringHeight/2, z);
+    
+            this.stringGroup.add(string);
+        }
+        this.basketGroup.add(this.stringGroup);
+
 
         this.groupBalloon.add(this.basketGroup);
+        this.add(this.groupBalloon);
     }
 }
 export { MyBalloon };
